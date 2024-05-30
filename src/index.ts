c3ba86938b41ef2d4c9c49ec30cf60301cf278dd
Torsten Irländer <torsten@irlaender.de>
2024-05-30 20:42:20 +0200

    chore: Call plugin locally

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import {MyCustomPlugin} from './lib';
+
 export * from './lib';
+
+async function runPlugin() {
+  const newModel = MyCustomPlugin({});
+  const usage = await newModel.execute([
+    {
+      timestamp: '2021-01-01T00:00:00Z',
+      duration: '15s',
+      'cpu-util': 34,
+    },
+    {
+      timestamp: '2021-01-01T00:00:15Z',
+      duration: '15s',
+      'cpu-util': 12,
+    },
+  ]);
+
+  console.log(usage);
+}
+
+runPlugin();
