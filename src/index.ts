e7286a9b142fb58bd16468247ab2d4f83684ad67
Torsten Irländer <torsten@irlaender.de>
2024-06-04 10:56:11 +0200

    refactor: Adapt init of Plugin

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import {MyCustomPlugin} from './lib';
+import {YourGlobalConfig} from './lib/my-custom-plugin/types';
 
 export * from './lib';
 
 async function runPlugin() {
-  const newModel = MyCustomPlugin({});
-  const usage = await newModel.execute([
-    {
-      timestamp: '2021-01-01T00:00:00Z',
-      duration: '15s',
-      'cpu-util': 34,
-    },
-    {
-      timestamp: '2021-01-01T00:00:15Z',
-      duration: '15s',
-      'cpu-util': 12,
-    },
-  ]);
-
+  const globalConfig: YourGlobalConfig = {
+    url: 'http://localhost:9090',
+  };
+  const newModel = MyCustomPlugin(globalConfig);
+  const usage = await newModel.execute([]);
   console.log(usage);
 }
 
